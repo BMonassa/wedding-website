acb5c8bb87e2808e0804a178e6962d572c35cdcd
Bruno <bruno-monassa@hotmail.com>
2023-12-27 10:50:12 -0300

    Adding img

--- src/view/GiftList/GiftList.tsx
+++ src/view/GiftList/GiftList.tsx
@@ -9,7 +9,7 @@ import Gift from '../../components/Gift';
 import Margin from '../../components/Margin';
 import TextCo from '../../components/TextCo';
 import Line from '../../components/Line';
-import giftList from './info';
+import giftListInfo from './info';
 
 export default function GiftList() {
 
@@ -37,60 +37,15 @@ export default function GiftList() {
         </ContainerLine>
 
         <ContainerGift>
-          <Gift
-            image={giftList.aluguel.image}
-            title={giftList.aluguel.title}
-            description={giftList.aluguel.description}
-            price={giftList.aluguel.price}
-          />
-          <Gift
-            image={giftList.buque.image}
-            title={giftList.buque.title}
-            description={giftList.buque.description}
-            price={giftList.buque.price}
-          />
-          <Gift
-            image={giftList.capacete.image}
-            title={giftList.capacete.title}
-            description={giftList.capacete.description}
-            price={giftList.capacete.price}
-          />
-          <Gift
-            image={giftList.rolo.image}
-            title={giftList.rolo.title}
-            description={giftList.rolo.description}
-            price={giftList.rolo.price}
-          />
-          <Gift
-            image={giftList.carroNoiva.image}
-            title={giftList.carroNoiva.title}
-            description={giftList.carroNoiva.description}
-            price={giftList.carroNoiva.price}
-          />
-          <Gift
-            image={giftList.maquiagem.image}
-            title={giftList.maquiagem.title}
-            description={giftList.maquiagem.description}
-            price={giftList.maquiagem.price}
-          />
-          <Gift
-            image={giftList.psicologo.image}
-            title={giftList.psicologo.title}
-            description={giftList.psicologo.description}
-            price={giftList.psicologo.price}
-          />
-          <Gift
-            image={giftList.idosos.image}
-            title={giftList.idosos.title}
-            description={giftList.idosos.description}
-            price={giftList.idosos.price}
-          />
-          <Gift
-            image={giftList.ressaca.image}
-            title={giftList.ressaca.title}
-            description={giftList.ressaca.description}
-            price={giftList.ressaca.price}
-          />
+          {Object.values(giftListInfo).map((gift, index) => (
+            <Gift
+              key={index}
+              image={gift.image}
+              title={gift.title}
+              description={gift.description}
+              price={gift.price}
+            />
+          ))}
 
         </ContainerGift>
       </ContainerWrapper>
--- src/view/GiftList/info.tsx
+++ src/view/GiftList/info.tsx
@@ -8,6 +8,12 @@ import idosos from '../../assets/image/Gift/idosos.jpg';
 import maquiagem from '../../assets/image/Gift/maquiagem.jpg';
 import psicologo from '../../assets/image/Gift/psicologo.jpg'
 import ressaca from '../../assets/image/Gift/ressaca.jpeg';
+import kitChurrasco from '../../assets/image/Gift/kitChurrasco.jpeg'
+import cobertor from '../../assets/image/Gift/cobertor.jpeg'
+import netflix from '../../assets/image/Gift/netflix.png'
+import miojo from '../../assets/image/Gift/miojo.jpeg'
+import pegarBuque from '../../assets/image/Gift/pegarBuque.jpeg'
+import cartoes from '../../assets/image/Gift/cartoes.jpeg'
 
 export default {
   aluguel: {
@@ -64,5 +70,40 @@ export default {
     description: 'Para o dia do casamento',
     price: 200
   },
+  kitChurrasco: {
+    image: kitChurrasco,
+    title: 'Kit Churrasco',
+    description: 'Para deixar a noiva alimentada',
+    price: 200
+  },
+  cobertor: {
+    image: cobertor,
+    title: 'Cobertor',
+    description: 'Para quando a noiva estiver coberta de razão',
+    price: 200
+  },
+  netflix: {
+    image: netflix,
+    title: 'Netflix',
+    description: 'Para os noivos assistir filme',
+    price: 200
+  },
+  miojo: {
+    image: miojo,
+    title: 'Miojo superfaturado',
+    description: 'Para deixar a noiva alimentada',
+    price: 200
+  },
+  pegarBuque: {
+    image: pegarBuque,
+    title: 'Aumente 30% de chance para pegar o buquê',
+    description: 'Para desencalhar',
+    price: 200
+  },
+  cartoes: {
+    image: cartoes,
+    title: 'Ajude a pagar o cartão',
+    description: 'Sério, da até medo de olhar a fatura',
+    price: 200
+  },
 };
-
